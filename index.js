dd9c4febe94260c8d009e53abdeac52635de1554
Gavin Dinubilo <gavincodes@gmail.com>
2016-04-24 04:01:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,36 @@
+// example
+'use strict'
+
 var Botkit = require('botkit')
 
-// Expect a SLACK_TOKEN environment variable
-var slackToken = process.env.SLACK_TOKEN
-if (!slackToken) {
-  console.error('SLACK_TOKEN is required!')
-  process.exit(1)
-}
-
 var controller = Botkit.slackbot()
-var bot = controller.spawn({
-  token: slackToken
+
+// Beepboop manages the hosting infrastructure for your bot and  publishes events
+// when a team adds, updates, or removes the bot, thereby enabling multitenancy
+// (multiple team instances of bot in one bot process). The beepboop-botkit package
+// listens for those events handles and starting/stopping the given team bot for you.
+// It is the develper's responsiblity to ensure any state stored outside of the configs
+// set in the project's bot.yml supports multitency (if you allow multiple teams to run your bot)
+var beepboop = require('../index.js')
+beepboop.start(controller, {
+  debug: true
 })
 
-bot.startRTM(function (err, bot, payload) {
-  if (err) {
-    throw new Error('Could not connect to Slack')
-  }
-})
-
+// Listen for botkit events
 controller.on('bot_channel_join', function (bot, message) {
-  bot.reply(message, "I'm here!")
+  bot.reply(message, 'I\'m here!')
 })
 
-controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
-  bot.reply(message, 'Hello.')
+controller.hears(['hi'], ['direct_message', 'direct_mention'], function (bot, evt) {
+  bot.reply(evt, 'hello from bot.js')
 })
 
-controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
-  bot.reply(message, 'Hello.')
-  bot.reply(message, 'It\'s nice to talk to you directly.')
+controller.hears(['config'], ['direct_message', 'direct_mention'], function (bot, evt) {
+  bot.reply(evt, 'CUSTOM_CONFIG_ITEM: ' + bot.config.CUSTOM_CONFIG_ITEM)
 })
 
-controller.hears('.*', ['mention'], function (bot, message) {
-  bot.reply(message, 'You really do care about me. :heart:')
-})
-
-controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
-  var help = 'I will respond to the following messages: \n' +
-      '`bot hi` for a simple message.\n' +
-      '`bot attachment` to see a Slack attachment message.\n' +
-      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
-      '`bot help` to see this again.'
-  bot.reply(message, help)
-})
-
-controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
-  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
-  var attachments = [{
-    fallback: text,
-    pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
-    title: 'Host, deploy and share your bot in seconds.',
-    image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-    title_link: 'https://beepboophq.com/',
-    text: text,
-    color: '#7CD197'
-  }]
-
-  bot.reply(message, {
-    attachments: attachments
-  }, function (err, resp) {
-    console.log(err, resp)
-  })
-})
-
-controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
+controller.hears(['help'], ['direct_message', 'direct_mention'], function (bot, evt) {
+  var help = 'I am an efficient little bot. All you need is a single instance of me to handle ' +
+      'multiple teams. If you run me on BeepBoop, I will even auto-scale! :boom:'
+  bot.reply(evt, help)
 })
